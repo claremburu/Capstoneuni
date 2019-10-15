2ffe8f8b738a6318451b38cdc9efef40b84fe088
claremburu <mburuclare8@gmail.com>
2019-10-15 12:38:49 +0300

    Put all dashboard componenets in the dashboard folder

--- src/client/App.js
+++ src/client/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { styles } from "../client/css/authstyles.css";
 
 import Dashboard from "./components/Dashboard/Dashboard"; // this is one of the many componets that you are gonna render here
 import PageNotfound from "./components/PageNotFound";
-import Donor from "./components/Donor/Donor";
-import User from "./components/User/User";
-import Admin from "./components/Admin/Admin";
 
 export default function App() {
   return (
@@ -13,9 +11,6 @@ export default function App() {
       {/* this is the everall div of your website e.g with className = "container" */}
       <Switch>
         <Route exact path="/" component={Dashboard} />
-        <Route path="/Donor" component={Donor} />
-        <Route path="/User" component={User} />
-        <Route path="/Admin" component={Admin} />
         <Route component={PageNotfound} />
       </Switch>
     </div>
--- src/client/components/Dashboard/Dashboard.jsx
+++ src/client/components/Dashboard/Dashboard.jsx
@@ -22,6 +22,12 @@ const Dashboard = () => {
         <li>
           <Link to="/Donor">Donor</Link>
         </li>
+        <li>
+          <Link to="/Admin">Admin</Link>
+        </li>
+        <li>
+          <Link to="/Auth">Auth</Link>
+        </li>
       </ul>
 
       <hr />
@@ -42,6 +48,10 @@ const Dashboard = () => {
         </Route>
         <Route path="/Donor">
           <Donor />
+        </Route>
+        <Route path="/Admin">
+        </Route>
+        <Route path="/Auth">
         </Route>
       </Switch>
     </div>
@@ -117,4 +127,416 @@ const Dashboard = () => {
 
     }
 
+    function User(){
+      return(
+          <div>
+      <div>
+  <title>Inter-Uni</title>
+  <link rel="stylesheet" href="../css/materialize.min.css" />
+  <nav className="blue">
+    <div className="nav-wrapper">
+      <a href="/" className="brand-logo">
+        Inter-Uni
+      </a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li>
+          <a href="user-dashboard.html">User</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div className="container white" style={{ minHeight: 800 }}>
+    <div className="row">
+      <div className="col s7">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s9">
+              <input id="search" type="email" className="validate" />
+              <label htmlFor="search">Search</label>
+            </div>
+            <div className="input-field col s3">
+              <button
+                id="search-btn"
+                type="button"
+                className="waves-effect waves-light btn"
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="col s12" id="projects-container">
+          <div className="card">
+            <div className="card-content">
+              <span className="card-title">Title</span>
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information. I am convenient because I require little markup to
+                use effectively.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <form className="col s4 offset-s1">
+        <h3>Add Project</h3>
+        <div className="row">
+          <div className="input-field col s12">
+            <input id="title" type="email" className="validate" />
+            <label htmlFor="title">Title</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s12">
+            <textarea
+              id="description"
+              className="materialize-textarea"
+              defaultValue={""}
+            />
+            <label htmlFor="description">Description</label>
+          </div>
+        </div>
+        <span>File</span>
+        <input type="file" name="document" />
+        <input type="submit" />
+      </form>
+      {/* <div class="file-field input-field">
+						<form method="POST" action="/submit-form">
+						<span>File</span>
+						<input type="file" name="document" />
+						<input type="submit" />
+					  </form>
+						
+						</div> */}
+      <div className="file-path-wrapper">
+        <input className="file-path validate" type="text" />
+      </div>
+    </div>
+    <button
+      id="add-project-btn"
+      type="button"
+      className="waves-effect waves-light btn"
+    >
+      Add Project
+    </button>
+    <button
+      id="add-project-btn"
+      type="button"
+      className="waves-effect waves-light btn"
+    >
+      View Funding opportunities
+    </button>
+    <button
+      id="add-project-btn"
+      type="button"
+      className="waves-effect waves-light btn"
+    >
+      Apply for funding
+    </button>
+  </div>
+  {/* Compiled and minified JavaScript */}
+</div>;
+        </div>
+      )
+    }
+
+    function Donor(){
+      return(
+        <div>
+           <div>
+  <title>Inter-Uni</title>
+  <link rel="stylesheet" href="../../css/materialize.min.css" />
+  <nav className="blue">
+    <div className="nav-wrapper">
+      <a href="/" className="brand-logo">
+        Inter-Uni
+      </a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li>
+          <a href="donor-dashboard.html">Donor</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div className="container white" style={{ minHeight: 800 }}>
+    <div className="row">
+      <div className="col s7">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s9">
+              <input id="search" type="email" className="validate" />
+              <label htmlFor="search">Search</label>
+            </div>
+            <div className="input-field col s3">
+              <button
+                id="search-btn"
+                type="button"
+                className="waves-effect waves-light btn"
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="col s12" id="projects-container">
+          <div className="card">
+            <div className="card-content">
+              <span className="card-title">Title</span>
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information. I am convenient because I require little markup to
+                use effectively.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <form className="col s4 offset-s1">
+        <h3>Add Advert</h3>
+        <div className="row">
+          <div className="input-field col s12">
+            <input id="title" type="text" className="validate" />
+            <label htmlFor="title">Title</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s12">
+            <textarea
+              id="description"
+              className="materialize-textarea"
+              defaultValue={""}
+            />
+            <label htmlFor="description">Description</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s12">
+            <input id="date" type="text" className="datepicker" />
+            <label htmlFor="title">Date</label>
+          </div>
+        </div>
+        {/* <div class="file-field input-field">
+						<div class="btn">
+							<span>File</span>
+							<input type="file" />
+						</div>
+						<div class="file-path-wrapper">
+							<input class="file-path validate" type="text" />
+						</div>
+					</div> */}
+        <button
+          id="add-project-btn"
+          type="button"
+          className="waves-effect waves-light btn"
+        >
+          Add Advert
+        </button>
+        <div>
+          <button
+            id="add-project-btn"
+            type="button"
+            className="waves-effect waves-light btn"
+          >
+            Funded Projects
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+  {/* Compiled and minified JavaScript */}
+</div>;
+
+        </div>
+      )
+    }
+    
+    function Admin(){
+      return(
+        <div>
+           <div>
+  <title>Inter-Uni</title>
+  <link rel="stylesheet" href="/CSS/materialize.min.css" />
+  <nav className="blue">
+    <div className="nav-wrapper">
+      <a href="/" className="brand-logo">
+        Inter-Uni
+      </a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li>
+          <a href="admin-dashboard.html">Admin</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div className="container white" style={{ minHeight: 800 }}>
+    <div className="row">
+      <div className="col s7">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s9">
+              <input id="search" type="email" className="validate" />
+              <label htmlFor="search">Search</label>
+            </div>
+            <div className="input-field col s3">
+              <button
+                id="search-btn"
+                type="button"
+                className="waves-effect waves-light btn"
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="col s12" id="projects-container">
+          <div className="card">
+            <div className="card-content">
+              <span className="card-title">Title</span>
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information. I am convenient because I require little markup to
+                use effectively.
+              </p>
+            </div>
+          </div>
+        </div>
+        <button
+          id="add-project-btn"
+          type="button"
+          className="waves-effect waves-light btn"
+        >
+          Verify Project
+        </button>
+      </div>
+    </div>
+  </div>
+  {/* Compiled and minified JavaScript */}
+</div>;
+        </div>
+      )
+    }
+
+    function Auth(){
+      return(
+        <div>
+          <div>
+  <title>Capstone Auth</title>
+  {/*Custom Theme files*/}
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="keywords" content />
+  {/* Custom Theme files */}
+  <link
+    href="CSS/authstyles.css"
+    rel="stylesheet"
+    type="text/css"
+    media="all"
+  />
+  {/*web-fonts*/}
+  <link
+    href="//fonts.googleapis.com/css?family=Jura:400,300,500,600"
+    rel="stylesheet"
+    type="text/css"
+  />
+  {/*//web-fonts*/}
+  <h1>User Login/Registration🚀 </h1>
+  {/* main */}
+  <div className="main">
+    {/*login-profile*/}
+    {/*login-profile*/}
+    {/*signin-form*/}
+    <div className="w3">
+      <div className="signin-form profile">
+        <h3>Login</h3>
+        <div className="login-form">
+          <form action="/" method="post">
+            <input type="text" name="logemail" placeholder="E-mail" required />
+            <input
+              type="password"
+              name="logpassword"
+              placeholder="Password"
+              required
+            />
+            <div className="tp">
+              <input type="button" defaultValue="LOGIN NOW" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div className="agile">
+      <div className="signin-form profile">
+        <h3>Register</h3>
+        <div className="login-form">
+          <form
+            id="login"
+            action="/"
+            method="post"
+            onsubmit="event.preventDefault(); validateMyForm();"
+          >
+            <p id="alert-el" />
+            <input
+              type="text"
+              name="first_name"
+              placeholder="First Name"
+              required
+            />
+            <input
+              type="text"
+              name="last_name"
+              placeholder="Last Name"
+              required
+            />
+            <input
+              type="text"
+              name="registration_number"
+              placeholder="RegNo"
+              required
+            />
+            <input
+              type="text"
+              name="university"
+              placeholder="University"
+              required
+            />
+            <input type="text" name="Course" placeholder="Course" required />
+            <input type="text" name="email" placeholder="E-mail" required />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              id="password"
+              required
+            />
+            <input
+              type="password"
+              name="passwordConf"
+              placeholder="Confirm Password"
+              id="password_conf"
+              required
+            />
+            <input type="submit" defaultValue="REGISTER" />
+          </form>
+        </div>
+        <p>
+          <a href="#"> By clicking register, I agree to your terms</a>
+        </p>
+      </div>
+    </div>
+    <div className="clear" />
+    {/*//signin-form*/}
+  </div>
+  <div className="copyright">
+    <p>
+      {" "}
+      © 2019 Capstone InterUni PM . All rights reserved | Design by{" "}
+      <a href="http://w3layouts.com/" target="_blank">
+        W3layouts
+      </a>
+    </p>
+  </div>
+</div>;
+        </div>
+      )
+    }
 export default Dashboard;
